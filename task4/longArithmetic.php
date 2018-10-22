b1349c0e8dc3a0fea15518b2f072f079359ac75c
KinAlex <cr0de4@gmail.com>
2018-10-23 00:29:29 +0300

    Add files via upload

---
+++ task4/longArithmetic.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 23.10.18
+ * Time: 0:27
+ */
+
+/**
+ * @param $a
+ * @param $b
+ * @return string
+ */
+function sum($a, $b) : string
+{
+    $totalNum = '';
+    $inMind = 0;
+    $strlenA = strlen($a);
+    $strlenB = strlen($b);
+    $shortest = $strlenA;
+    $longString = $b;
+    $diff = $strlenB - $shortest;
+    if (($strlenA <=> $strlenB) === 1) {
+        $shortest = $strlenB;
+        $diff = $strlenA - $shortest;
+        $longString = $a;
+    }
+
+    for ($i = 1; $i < $shortest + 1; $i++) {
+        $sumTemp = $a[$strlenA - $i] + $b[$strlenB - $i] + $inMind;
+        if ($sumTemp > 9) {
+            $ost = $sumTemp - 10;
+            $totalNum = $ost.$totalNum;
+            $inMind = 1;
+        } else {
+            $tempSumWithMind = $sumTemp + $inMind;
+            $inMind = 0;
+            if ($tempSumWithMind > 9) {
+                $ost = $sumTemp - 9;
+                $totalNum = $ost.$totalNum;
+                $inMind = 1;
+            }
+            $totalNum = $tempSumWithMind.$totalNum;
+        }
+    }
+
+    if ($inMind) {
+        if ($strlenA === $strlenB) {
+            return '1'.$totalNum;
+        }
+
+        for ($i = $diff - 1; $i >= 0; $i--) {
+            $tmpNum = $longString[$i] + 1;
+            if ($tmpNum + 1 === 10) {
+                $totalNum = '0'.$totalNum;
+            } else {
+                $totalNum = substr($longString, 0, $diff - 1) . $tmpNum . $totalNum;
+                $inMind = 0;
+                break;
+            }
+        }
+        if($inMind) {
+            $totalNum = '1'.$totalNum;
+        }
+
+        return $totalNum;
+    }
+
+    return $totalNum;
+}
